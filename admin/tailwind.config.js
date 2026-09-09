2b9956497222ff3d227ed0659410fa1179aed720
Nihal MV <nihalmv.dev@gmail.com>
2026-09-09 18:20:49 +0530

    fix(admin): move no-scrollbar utility to tailwind config to eliminate scrollbar-width css warning

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -44,6 +44,18 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar::-webkit-scrollbar': {
+          'display': 'none'
+        },
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none'
+        }
+      });
+    }
+  ],
 }
 
